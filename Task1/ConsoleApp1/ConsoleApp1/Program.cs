5742bc3a272c873d3380a42626b04ce15c3349f3
TahaSavas <taha.savas@hotmail.com>
2020-11-01 21:43:13 +0100

    Added

---
+++ Task1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+         public static async Task Main(string[] args)
+        {
+            var websiteUrl = args[0];
+            var httpClient = new HttpClient();
+            var respnse = await httpClient.GetAsync(websiteUrl);
+            if (respnse.IsSuccessStatusCode)
+            {
+                var htmlContent = await respnse.Content.ReadAsStringAsync();
+
+                var regex = new Regex("[a-z0-9]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                var emailAddresses = regex.Matches(htmlContent);
+                foreach (var match in emailAddresses)
+                {
+                    Console.WriteLine(match.ToString());
+
+                    Console.WriteLine("hello");
+                }
+            }
+        }
+    }
+}
